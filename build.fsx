bb033b7c309d9871f51ec33aa9a893825902f8e3
krauthaufen <krauthaufen@awx.at>
2015-03-20 09:20:04 +0100

    some updates for build script;

--- build.fsx
+++ build.fsx
@@ -5,9 +5,65 @@ open Fake
 open System
 open System.IO
 
-let core = !!"src/**/*.fsproj" ++ "src/**/*.csproj";
+let core = ["src/Aardvark.Rendering.sln"];
 
-printfn "%A" (core |> Seq.toList)
+module Std =
+    open System.Runtime.InteropServices
+
+
+    module private Kernel32 =
+        let StdOutputHandle = 0xFFFFFFF5u
+
+        [<DllImport("kernel32.dll")>]
+        extern IntPtr GetStdHandle(UInt32 nStdHandle)
+
+        [<DllImport("kernel32.dll")>]
+        extern void SetStdHandle(UInt32 nStdHandle, IntPtr handle)
+
+        let getstdout() = GetStdHandle(StdOutputHandle)
+        let setstdout(h) = SetStdHandle(StdOutputHandle, h)
+
+    module private Libc =
+//        let STDOUT_FILENO = 1
+//
+//        [<DllImport("libc")>]
+//        extern int dup(int oldfd)
+//
+//        [<DllImport("libc")>]
+//        extern int dup2(int oldfd, int newfd)
+
+        let getstdout() = 0n; //dup(STDOUT_FILENO) |> nativeint
+        let setstdout(h : nativeint) = () //dup2(int h, STDOUT_FILENO) |> ignore
+
+    let getstdout =
+        match Environment.OSVersion.Platform with
+            | PlatformID.Unix -> Libc.getstdout
+            | _ -> Kernel32.getstdout
+
+    let setstdout =
+        match Environment.OSVersion.Platform with
+            | PlatformID.Unix -> Libc.setstdout
+            | _ -> Kernel32.setstdout
+
+    let noOut (f : unit -> 'a) =
+        let outHandle = getstdout()
+        let out = Console.Out
+
+        let success = 
+            try
+                setstdout(0n)
+                Console.SetOut TextWriter.Null
+                true
+            with e ->
+                false
+
+        let res = f()
+
+        if success then
+            Console.SetOut out
+            setstdout(outHandle)
+
+        res
 
 Target "Restore" (fun () ->
 
@@ -37,9 +93,19 @@ Target "Default" (fun () -> ())
     "Compile" ==>
     "Default"
 
-let knownPackages = 
+let ownPackages = 
     Set.ofList [
         "Aardvark.Rendering"
+    ]
+
+let subModulePackages =
+    Map.ofList [
+        "src/Aardvark.Base", [
+            "Aardvark.Base"
+            "Aardvark.Base.FSharp"
+            "Aardvark.Base.Essentials"
+            "Aardvark.Base.Incremental"
+        ]
     ]
 
 
@@ -50,13 +116,13 @@ Target "CreatePackage" (fun () ->
     if branch = "master" then
         let tag = Fake.Git.Information.getLastTag()
 
-        for id in knownPackages do
+        for id in ownPackages do
             NuGetPack (fun p -> 
                 { p with OutputPath = "bin"; 
                          Version = tag; 
                          ReleaseNotes = releaseNotes; 
                          WorkingDir = "bin"
-                         Dependencies = p.Dependencies |> List.map (fun (id,version) -> if Set.contains id knownPackages then (id, tag) else (id,version)) 
+                         Dependencies = p.Dependencies |> List.map (fun (id,version) -> if Set.contains id ownPackages then (id, tag) else (id,version)) 
                 }) (sprintf "bin/%s.nuspec" id)
     
     else 
@@ -77,7 +143,7 @@ Target "Deploy" (fun () ->
         match accessKey with
             | Some accessKey ->
                 try
-                    for id in knownPackages do
+                    for id in ownPackages do
                         NuGetPublish (fun p -> 
                             { p with 
                                 Project = id
@@ -85,7 +151,7 @@ Target "Deploy" (fun () ->
                                 Version = tag; 
                                 ReleaseNotes = releaseNotes; 
                                 WorkingDir = "bin"
-                                Dependencies = p.Dependencies |> List.map (fun (id,version) -> if Set.contains id knownPackages then (id, tag) else (id,version)) 
+                                Dependencies = p.Dependencies |> List.map (fun (id,version) -> if Set.contains id ownPackages then (id, tag) else (id,version)) 
                                 AccessKey = accessKey
                                 Publish = true
                             })
@@ -96,6 +162,76 @@ Target "Deploy" (fun () ->
      else 
         traceError (sprintf "cannot deploy branch: %A" branch)
 )
+
+// installs local packages specified by subModulePackages 
+// NOTE: current packages will always be replaced
+Target "InstallLocal" (fun () ->
+
+    let buildCmdName =
+        match Environment.OSVersion.Platform with
+            | PlatformID.Unix 
+            | PlatformID.MacOSX -> "build.sh"
+            | _ -> "build.cmd"
+
+    for (localModulePath, packages) in Map.toSeq subModulePackages do
+        let modulePath = Path.GetFullPath localModulePath
+        if Directory.Exists modulePath then
+
+            let rec findPackagePath l =
+                match l with
+                    | [] -> None
+                    | p::ps -> 
+                        let p = Path.Combine(modulePath, p)
+                        if Directory.Exists p then Some p
+                        else findPackagePath ps
+
+            let buildCmd = Path.Combine(modulePath, buildCmdName)
+
+            let packageOutputPath = findPackagePath ["bin"; "build"]
+
+            match packageOutputPath with
+                | Some packageOutputPath ->
+                    if File.Exists buildCmd then
+                        let ret = 
+                            ExecProcess (fun info -> 
+                                info.UseShellExecute <- true
+                                info.CreateNoWindow <- false
+                                info.FileName <- buildCmd
+                                info.Arguments <- "CreatePackage"
+                                info.WorkingDirectory <- modulePath
+                            ) TimeSpan.MaxValue
+
+
+                        if ret = 0 then
+                            let packagePath = Path.Combine(modulePath, "packages")
+
+                            for p in packages do
+                                try
+                                    let outputFolders = Directory.GetDirectories("packages", p + ".*")
+                                    for outputFolder in outputFolders do
+                                        Directory.Delete(outputFolder, true)
+
+                                    Std.noOut (fun () ->
+                                        RestorePackageId (fun p -> { p with Sources = [packageOutputPath; packagePath]; OutputPath = "packages"; }) p
+                                    )
+                                    trace (sprintf "successfully reinstalled %A" p)
+
+                                with :? UnauthorizedAccessException as e ->
+                                    traceImportant (sprintf "could not reinstall %A" p  )
+
+
+                        else
+                            traceError (sprintf "build failed for submodule: %A" localModulePath)
+                    else
+                        traceError (sprintf "could not locate build.cmd in submodule %A" localModulePath)
+                | _ ->
+                    traceError (sprintf "could not locate output folder in submodule %A" localModulePath)
+        else
+            trace (sprintf "could not locate submodule %A" localModulePath)
+
+    ()
+)
+
 
 "Compile" ==> "CreatePackage"
 "CreatePackage" ==> "Deploy"
