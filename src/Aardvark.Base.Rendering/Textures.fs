889c6371a478edea47687f694895b05dd814f5bc
Harald Steinlechner <hs@vrvis.at>
2015-07-27 16:46:36 +0200

    added defaultTextures module

--- src/Aardvark.Base.Rendering/Textures.fs
+++ src/Aardvark.Base.Rendering/Textures.fs
@@ -89,3 +89,20 @@ type PixTexture3d(data : PixVolume, wantMipMaps : bool) =
                 data = o.PixVolume && wantMipMaps = o.WantMipMaps
             | _ ->
                 false
+
+
+module DefaultTextures =
+
+    let checkerboardPix = 
+        let pi = PixImage<byte>(Col.Format.RGBA, V2i.II * 256)
+        pi.GetMatrix<C4b>().SetByCoord(fun (c : V2l) ->
+            let c = c / 16L
+            if (c.X + c.Y) % 2L = 0L then
+                C4b.White
+            else
+                C4b.Gray
+        ) |> ignore
+        pi
+
+    let checkerboard = 
+        PixTexture2d(PixImageMipMap [| checkerboardPix :> PixImage |], true) :> ITexture |> Mod.constant
