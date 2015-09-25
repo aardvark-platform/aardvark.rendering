8e6f99ac68a8004df7194905f7d7ab3ae4a552e0
krauthaufen <krauthaufen@awx.at>
2015-09-25 17:59:29 +0200

    removed GL Stopwatch from ReseourceSet;

--- src/Aardvark.Rendering.GL/Runtime/Compiler/ChangeSets.fs
+++ src/Aardvark.Rendering.GL/Runtime/Compiler/ChangeSets.fs
@@ -85,7 +85,6 @@ type ResourceSet(addInput : IAdaptiveObject -> unit, removeInput : IAdaptiveObje
     let set = HashSet<IChangeableResource>()
     let callbacks = Dictionary<IChangeableResource, (unit -> unit)>()
     let sw = System.Diagnostics.Stopwatch()
-    let glsw = OpenGlStopwatch()
 
     let dirty (m : IChangeableResource) () =
         lock l (fun () -> 
@@ -139,7 +138,6 @@ type ResourceSet(addInput : IAdaptiveObject -> unit, removeInput : IAdaptiveObje
                 dirtySet
             )
         sw.Restart()
-        glsw.Restart()
         let mutable count = 0
 
         let mutable counts = Map.empty
@@ -157,7 +155,6 @@ type ResourceSet(addInput : IAdaptiveObject -> unit, removeInput : IAdaptiveObje
 
 
         if Config.SyncUploadsAndFrames then OpenTK.Graphics.OpenGL4.GL.Sync()
-        glsw.Stop()
         sw.Stop()
 
         //printfn "GL: %fms vs .NET: %fms" glsw.Elapsed.TotalMilliseconds sw.Elapsed.TotalMilliseconds 
