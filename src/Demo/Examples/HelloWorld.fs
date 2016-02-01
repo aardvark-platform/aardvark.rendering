a7657b2726b18f21d95905d0b07ee54989711266
Harald Steinlechner <hs@vrvis.at>
2016-02-01 16:41:58 +0100

    Examples/Lod: sync

--- src/Demo/Examples/HelloWorld.fs
+++ src/Demo/Examples/HelloWorld.fs
@@ -611,7 +611,7 @@ module HelloWorld =
         use app = new OpenGlApplication()
         let win = app.CreateSimpleRenderWindow(1)
    
-        let view = CameraView.LookAt(V3d(150.0,150.0,150.0), V3d(50,50,50), V3d.OOI)
+        let view = CameraView.LookAt(V3d(10.0,10.0,10.0), V3d(0,0,0), V3d.OOI)
         let perspective = 
             win.Sizes 
               |> Mod.map (fun s -> Frustum.perspective 60.0 0.1 1000.0 (float s.X / float s.Y))
@@ -619,14 +619,15 @@ module HelloWorld =
 
         let viewTrafo = DefaultCameraController.control win.Mouse win.Keyboard win.Time view
 
+        let colors = [| C4b.Gray; C4b.Green; C4b.Red; C4b.VRVisGreen; C4b.Blue; |]
+
         let rand = Random()
-        let randomPoints (bounds : Box3d) pointCount =
+        let randomPoints (bounds : Box3d) level pointCount =
             let randomV3f() = V3d(rand.NextDouble(), rand.NextDouble(), rand.NextDouble()) * bounds.Size + bounds.Min |> V3f.op_Explicit
-            let randomColor() = C4b(rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), 1.0)
+            let randomColor() = colors.[level]// C4b(rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), 1.0)
 
             IndexedGeometry(
                 Mode = IndexedGeometryMode.PointList,
-                IndexArray = Array.init pointCount id,
                 IndexedAttributes = 
                     SymDict.ofList [
                          DefaultSemantic.Positions, Array.init pointCount (fun _ -> randomV3f()) :> Array
@@ -636,29 +637,28 @@ module HelloWorld =
 
         let scene = 
             aset {
-                for x in 0 .. 10 do
-                    for y in 0 .. 10 do
-                        for z in 0 .. 3 do
+                for x in 0 .. 20 do
+                    for y in 0 .. 20 do
+                        for z in 0 .. 20 do
                             let bounds = Box3d.FromMinAndSize(V3d(x,y,z), V3d.III)
-                            yield { bounds = bounds; levels = 3; createLevel = fun level -> randomPoints bounds ((1 <<< level)*10) }
+                            yield { bounds = bounds; levels = 3; createLevel = fun level -> randomPoints bounds level ((1 <<< level)*5) }
             }
 
         let getLodRepr (vt : IMod<CameraView>) (proj : IMod<Frustum>) (g : Geometry) : IMod<IndexedGeometry> =
             let mutable current = None
             adaptive  {
-                let proj = proj
                 let! vt = vt
                 let level = 
                     match V3d.Distance(g.bounds.GetClosestPointOn vt.Location, vt.Location) with
-                    | v when v < 5.0 -> 3
-                    | v when v < 10.0 -> 2
-                    | v when v < 50.0 -> 1
+                    | v when v < 4.0 -> 0
+                    | v when v < 10.0 -> 0
+                    | v when v < 20.0 -> 0
                     | _ -> 0
                 match current with 
                  | Some (oldLevel,g) when oldLevel = level -> 
                     return g
                  | _ -> 
-                    printfn "%A" level
+                    //printfn "%A" level
                     let g = g.createLevel level
                     current <- Some (level,g)
                     return g
@@ -672,8 +672,8 @@ module HelloWorld =
 
         let sg = 
             scene |> ASet.mapM ( getLodRepr viewTrafo perspective ) 
-            //|> Sg.geometrySet IndexedGeometryMode.PointList attributeTypes
-            |> ASet.map Sg.ofIndexedGeometry |> Sg.set
+            |> Sg.geometrySet IndexedGeometryMode.PointList attributeTypes
+            //|> ASet.map Sg.ofIndexedGeometry |> Sg.set
 
 
         let final =
