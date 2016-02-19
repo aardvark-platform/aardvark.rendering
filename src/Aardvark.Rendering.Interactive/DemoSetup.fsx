5008d3131a69c14e08887a0bc44c5031b79b5657
Harald Steinlechner <hs@vrvis.at>
2016-02-19 08:54:15 +0100

    added Aardvark.Rendering.Interactive

---
+++ src/Aardvark.Rendering.Interactive/DemoSetup.fsx
@@ -0,0 +1,41 @@
+﻿
+#I @"../../bin/Debug"
+#I @"../../bin/Release"
+
+#load "LoadReferences.fsx"
+
+open Aardvark.Rendering.Interactive
+
+open System
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open Aardvark.SceneGraph
+open Aardvark.Application
+
+open Default // makes viewTrafo and other tutorial specicific default creators visible
+Aardvark.Rendering.Interactive.FsiSetup.init (Path.combine [__SOURCE_DIRECTORY__; ".."; ".."; "bin";"Debug"])
+
+let quadSg =
+    let quad =
+        let index = [|0;1;2; 0;2;3|]
+        let positions = [|V3f(-1,-1,0); V3f(1,-1,0); V3f(1,1,0); V3f(-1,1,0) |]
+
+        IndexedGeometry(IndexedGeometryMode.TriangleList, index, SymDict.ofList [DefaultSemantic.Positions, positions :> Array], SymDict.empty)
+
+    // create a scenegraph given the indexedGeometry containing a quad
+    quad |> Sg.ofIndexedGeometry
+
+let sg =
+    quadSg |> Sg.effect [
+            DefaultSurfaces.trafo |> toEffect                   // compose shaders by using FShade composition.
+            DefaultSurfaces.constantColor C4f.Red |> toEffect   // use standard trafo + map a constant color to each fragment
+            ]
+        // viewTrafo () creates camera controls and returns IMod<ICameraView> which we project to its view trafo component by using CameraView.viewTrafo
+        |> Sg.viewTrafo (viewTrafo   () |> Mod.map CameraView.viewTrafo ) 
+        // perspective () connects a proj trafo to the current main window (in order to take account for aspect ratio when creating the matrices.
+        // Again, perspective() returns IMod<Frustum> which we project to its matrix by mapping ofer Frustum.projTrafo.
+        |> Sg.projTrafo (perspective () |> Mod.map Frustum.projTrafo    )
+
+setSg sg
+
+printfn "Done. Modify sg and call setSg again in order to see the modified rendering result."
---
+++ src/Aardvark.Rendering.Interactive/FsiSetup.fs
@@ -0,0 +1,117 @@
+﻿namespace Aardvark.Rendering.Interactive
+
+[<AutoOpen>]
+module FsiSetup =
+
+
+    open System
+    open System.Threading
+
+    open Aardvark.Base
+    open Aardvark.Base.Incremental
+    open Aardvark.Base.Rendering
+    open Aardvark.Rendering.NanoVg
+    open Aardvark.SceneGraph
+    open Aardvark.SceneGraph.Semantics
+
+    open Aardvark.Application
+    open Aardvark.Application.WinForms
+
+    let mutable private initialized = 0
+
+    let private winFormsApp = lazy ( new OpenGlApplication() )
+
+
+
+
+    let init entryDir =
+        let entry = "Aardvark.Rendering.Interactive.dll"
+        let entryPath = Path.combine [entryDir; entry] 
+        if entryPath |> System.IO.File.Exists then
+            printfn "using %s as entry assembly." entryPath
+        else failwithf "could not find entry assembly: %s" entryPath
+        
+        if Interlocked.Exchange(&initialized, 1) = 0 then
+            System.Environment.CurrentDirectory <- entryDir
+            IntrospectionProperties.CustomEntryAssembly <- 
+                System.Reflection.Assembly.LoadFile(entryPath)
+
+            Ag.initialize()
+            Aardvark.Base.Ag.unpack <- fun o ->
+                    match o with
+                        | :? IMod as o -> o.GetValue(null)
+                        | _ -> o
+
+            Aardvark.Init()
+ 
+    let openWindow() =
+        if initialized = 0 then failwith "first use Aardvark.Rendering.Interactive.init(pathToBuild)"
+
+        let app = winFormsApp.Value
+        let win = app.CreateSimpleRenderWindow(1)
+        let task = app.Runtime.CompileRender(win.FramebufferSignature, Sg.group [])
+
+        win.Text <- @"Aardvark rocks \o/"
+        win.Visible <- true 
+        win.RenderTask <- task
+
+        win :> IRenderControl
+
+    let closeWindow (c : IRenderControl) =
+        match c with
+            | :? SimpleRenderWindow as w ->
+                w.Close()
+                w.RenderTask.Dispose()
+                w.Dispose()
+            | _ ->
+                ()
+
+    let showSg (w : IRenderControl) (sg : ISg) =
+        if initialized = 0 then failwith "first use Aardvark.Rendering.Interactive.init(pathToBuild)"
+
+        try
+            let task = w.Runtime.CompileRender(w.FramebufferSignature, sg)
+
+            let old = w.RenderTask
+            w.RenderTask <- task
+            
+            match w with
+                | :? SimpleRenderWindow as w ->
+                    w.Invalidate()
+                | _ -> ()
+
+            old.Dispose()
+        with e ->
+            Log.warn "failed to set sg: %A" e
+
+    let clearSg (w : IRenderControl) =
+        showSg w (Sg.group [])
+
+    let viewTrafo (win : IRenderControl) =
+        let view =  CameraView.LookAt(V3d(3.0, 3.0, 3.0), V3d.Zero, V3d.OOI)
+        DefaultCameraController.control win.Mouse win.Keyboard win.Time view
+
+    let perspective (win : IRenderControl) = 
+        win.Sizes |> Mod.map (fun s -> Frustum.perspective 60.0 0.01 10.0 (float s.X / float s.Y))
+
+
+    let runInteractive () =
+          
+        if initialized = 0 then failwith "first use Aardvark.Rendering.Interactive.init(pathToBuild)"
+
+        let app = new OpenGlApplication()
+        let win = app.CreateSimpleRenderWindow(1)
+        let root = Mod.init <| (Sg.group [] :> ISg)
+
+
+        let task = app.Runtime.CompileRender(win.FramebufferSignature, Sg.DynamicNode root) //|> DefaultOverlays.withStatistics
+
+        win.Text <- @"Aardvark rocks \o/"
+        win.Visible <- true
+
+        let fixupRenderTask () =
+            if win.RenderTask = Unchecked.defaultof<_> then win.RenderTask <- task
+
+        (fun s -> fixupRenderTask () ; transact (fun () -> Mod.change root s)), win, task
+      
+    
