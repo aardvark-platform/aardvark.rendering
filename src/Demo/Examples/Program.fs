5482e3168543ab5e4c2ab139840a28ce15e0465c
Harald Steinlechner <hs@vrvis.at>
2016-03-18 15:26:33 +0100

    updated to f# 4.0

--- src/Demo/Examples/Program.fs
+++ src/Demo/Examples/Program.fs
@@ -15,8 +15,8 @@ let main args =
     //Examples.PostProcessing.run()
     //Examples.Shadows.run()
     //Examples.GeometrySet.run()
-    //Examples.LoD.run()
-    Examples.Polygons.run()
+    Examples.LoD.run()
+    //Examples.Polygons.run()
     //Examples.TicTacToe.run()
     //Examples.AssimpInterop.run()
     0
