b5be3eaf040e7f381219eca23c2bfc6cff60e9bf
Christian Luksch <luksch@vrvis.at>
2016-05-03 11:27:59 +0200

    added clipping to thickLine surface (fixes misplaced lines when one point is behind the camera);

--- src/Aardvark.Base.Rendering/DefaultSurfaces.fs
+++ src/Aardvark.Base.Rendering/DefaultSurfaces.fs
@@ -57,15 +57,21 @@ module DefaultSurfaces =
         [<Semantic("Width")>]       w       : float
     }
 
+    let Lerp (a : V4d) (b : V4d) (s : float) : V4d = failwith ""
+
     let thickLine (line : Line<ThickLineVertex>) =
         triangle {
             let t = uniform.LineWidth
             let sizeF = V3d(float uniform.ViewportSize.X, float uniform.ViewportSize.Y, 1.0)
 
-            let w0 = max line.P0.pos.W 0.001
-            let w1 = max line.P1.pos.W 0.001
-            let p0 = line.P0.pos.XYZ / w0
-            let p1 = line.P1.pos.XYZ / w1
+            let pp0 = line.P0.pos
+            let pp1 = line.P1.pos
+
+            let pp0 = if pp0.Z < 0.0 then (Lerp pp1 pp0 (pp1.Z / (pp1.Z - pp0.Z))) else pp0
+            let pp1 = if pp1.Z < 0.0 then (Lerp pp0 pp1 (pp0.Z / (pp0.Z - pp1.Z))) else pp1
+
+            let p0 = pp0.XYZ / pp0.W
+            let p1 = pp1.XYZ / pp1.W
 
             let fwp = (p1.XYZ - p0.XYZ) * sizeF
 
@@ -80,9 +86,9 @@ module DefaultSurfaces =
             let rel = t / (Vec.length fwp)
 
             yield { line.P0 with pos = V4d(p00, 1.0); lc = V2d(-1.0, -rel); w = rel }
-            yield { line.P0 with pos = V4d(p10, 1.0); lc = V2d(1.0, -rel); w = rel }
+            yield { line.P0 with pos = V4d(p10, 1.0); lc = V2d( 1.0, -rel); w = rel }
             yield { line.P1 with pos = V4d(p01, 1.0); lc = V2d(-1.0, 1.0 + rel); w = rel }
-            yield { line.P1 with pos = V4d(p11, 1.0); lc = V2d(1.0, 1.0 + rel); w = rel }
+            yield { line.P1 with pos = V4d(p11, 1.0); lc = V2d( 1.0, 1.0 + rel); w = rel }
 
         }
         
