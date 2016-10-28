601a3bed516156ef4148106439245538ac892f24
krauthaufen <krauthaufen@awx.at>
2016-10-28 10:19:47 +0200

    [GL] new streaming texture implementation

--- src/Aardvark.Rendering.GL/Resources/StreamingTexture.fs
+++ src/Aardvark.Rendering.GL/Resources/StreamingTexture.fs
@@ -15,7 +15,7 @@ open Aardvark.Rendering.GL
 
 type private PixelBuffer = { handle : int; mutable sizeInBytes : int64 }
 
-type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
+type StreamingTextureOld(ctx : Context, handle : int, mipMap : bool) =
     inherit AdaptiveObject()
 
     let expectedLevels (size : V2i) = 
@@ -103,7 +103,7 @@ type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
             | _ ->
                 failwithf "unsupported format: %A" fmt
 
-    static let mark (self : StreamingTexture) =
+    static let mark (self : StreamingTextureOld) =
         let action = 
             lock self (fun () ->
                 if not self.OutOfDate then
@@ -117,7 +117,7 @@ type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
                             fun () -> t.Commit()
                 else id
             )
-        action()
+        async { action() }
 
 
     member x.Context = ctx
@@ -137,7 +137,8 @@ type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
 
     interface IStreamingTexture with
         member x.Update(fmt, size, data) = x.Update(fmt, size, data)
-        member x.ReadPixel(pos) = x.ReadPixel pos
+        member x.UpdateAsync(fmt, size, data) = x.Update(fmt, size, data); new Transaction()
+        member x.ReadPixel(pos) = x.ReadPixel(pos).ToC4f()
 
     member x.Update(f : PixFormat, s : V2i, data : nativeint) =
         let pbo = swapPBOs()
@@ -146,7 +147,7 @@ type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
                 uploadUsingPBO pbo f s data
             )
         )
-        mark x
+        mark x |> Async.RunSynchronously
 
     member x.ReadPixel(pos : V2i) =
         let pbo = lastPBO()
@@ -188,6 +189,224 @@ type StreamingTexture(ctx : Context, handle : int, mipMap : bool) =
 
             pixelBuffers <- null
 
+type StreamingTexture(ctx : Context, mipMap : bool) =
+    inherit Mod.AbstractMod<ITexture>()
+
+    let expectedLevels (size : V2i) = 
+        if mipMap then Fun.Min(size.X, size.Y) |> Fun.Log2 |> Fun.Ceiling |> int
+        else 1
+
+    let mutable pbo = 0
+    let mutable pboSize = 0n
+   
+
+    let mutable ping = 
+        use t = ctx.ResourceLock
+        let handle = GL.GenTexture()
+        Texture(ctx, handle, TextureDimension.Texture2D, 1, 1, V3i.Zero, 1, TextureFormat.Rgba8, 0L, false)
+
+    let mutable pong = 
+        use t = ctx.ResourceLock
+        let handle = GL.GenTexture()
+        Texture(ctx, handle, TextureDimension.Texture2D, 1, 1, V3i.Zero, 1, TextureFormat.Rgba8, 0L, false)
+
+    let mutable currentFormat = PixFormat(typeof<obj>, Col.Format.RGBA)
+    let mutable currentSize = -V2i.II
+
+    let mutable textureFormat = TextureFormat.Alpha
+    let mutable pixelType = PixelType.UnsignedByte
+    let mutable pixelFormat = PixelFormat.Alpha
+    let mutable channels = 0
+    let mutable channelSize = 0
+
+    let mutable mipMapLevels = 0
+    let mutable bufferSize = 0n
+
+    let pboLock = obj()
+
+    let upload (f : PixFormat) (size : V2i) (data : nativeint) =
+        use token = ctx.ResourceLock
+
+        // update format depenent things
+        if f <> currentFormat then
+            currentFormat <- f
+            textureFormat <- TextureFormat.ofPixFormat f TextureParams.empty
+            let pf, pt = TextureFormat.toFormatAndType textureFormat
+            pixelType <- toPixelType f.Type |> Option.get
+            pixelFormat <- toPixelFormat f.Format |> Option.get
+            channels <- PixelFormat.channels pf
+            channelSize <- PixelType.size pt
+
+        // update size depenent things
+        if currentSize <> size then
+            currentSize <- size
+            mipMapLevels <- expectedLevels size
+            bufferSize <- size.X * size.Y * channels * channelSize |> nativeint
+
+        lock pboLock (fun () ->
+            // create the pbo if necessary
+            if pbo = 0 then pbo <- GL.GenBuffer()
+            GL.BindBuffer(BufferTarget.PixelUnpackBuffer, pbo)
+            GL.Check "could not bind PBO"
+
+            // update its size if necessary
+            if pboSize <> bufferSize then
+                pboSize <- bufferSize
+                GL.BufferData(BufferTarget.PixelUnpackBuffer, bufferSize, 0n, BufferUsageHint.DynamicDraw)
+                GL.Check "could not resize PBO"
+
+            // upload the data to the pbo
+            let ptr = GL.MapBufferRange(BufferTarget.PixelUnpackBuffer, 0n , bufferSize, BufferAccessMask.MapWriteBit)
+            if ptr = 0n then failwithf "[GL] could not map PBO"
+            Marshal.Copy(data, ptr, bufferSize)
+            if not (GL.UnmapBuffer(BufferTarget.PixelUnpackBuffer)) then
+                failwithf "[GL] could not unmap PBO"
+        )
+
+        // copy pbo to texture
+        GL.BindTexture(TextureTarget.Texture2D, ping.Handle)
+        GL.Check "could not bind texture"
+
+
+        if ping.Size2D = size && ping.Format = textureFormat && ping.MipMapLevels = mipMapLevels then
+            GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, size.X, size.Y, pixelFormat, pixelType, 0n)
+            GL.Check "could not update texture"
+        else
+            GL.TexImage2D(TextureTarget.Texture2D, 0, unbox (int textureFormat), size.X, size.Y, 0, pixelFormat, pixelType, 0n)
+            GL.Check "could not update texture"
+            ping.Size <- V3i(size.X, size.Y, 1)
+            ping.Format <- textureFormat
+            ping.MipMapLevels <- mipMapLevels
+            ping.ImmutableFormat <- false
+            ping.Count <- 1
+            ping.Dimension <- TextureDimension.Texture2D
+            ping.Multisamples <- 1
+
+            let newSize = if mipMap then (int64 bufferSize * 4L) / 3L else int64 bufferSize
+            updateTexture ctx ping.SizeInBytes newSize
+            ping.SizeInBytes <- newSize
+
+
+        if mipMapLevels > 1 then 
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D)
+            GL.Check "could not generate mipmaps"
+        GL.BindTexture(TextureTarget.Texture2D, 0)
+        GL.Check "could not unbind texture"
+
+        // unbind the pbo
+        GL.BindBuffer(BufferTarget.PixelUnpackBuffer, 0)
+        GL.Check "could not unbind PBO"
+
+        Fun.Swap(&ping, &pong)
+
+    member x.Context = ctx
+
+    member x.UpdateAsync(f : PixFormat, size : V2i, data : nativeint) =
+        upload f size data
+        let t = new Transaction()
+        t.Enqueue x
+        t
+
+    member x.Update(f : PixFormat, size : V2i, data : nativeint) =
+        upload f size data
+        
+        transact (fun () -> x.MarkOutdated())
+
+    member x.ReadPixel(pos : V2i) =
+        lock pboLock (fun () ->
+            if pbo = 0 || pos.X < 0 || pos.Y < 0 || pos.X >= currentSize.X || pos.Y >= currentSize.Y then
+                C4f(0.0f, 0.0f, 0.0f, 0.0f)
+            else
+                use token = ctx.ResourceLock
+
+                let pixelSize = channels * channelSize
+                let offset = pixelSize * (pos.X + pos.Y * currentSize.X)
+            
+                let data : byte[] = Array.zeroCreate pixelSize
+                let gc = GCHandle.Alloc(data, GCHandleType.Pinned)
+            
+                try
+                    GL.BindBuffer(BufferTarget.CopyReadBuffer, pbo)
+                    GL.Check "could not bind PBO"
+
+                    GL.GetBufferSubData(BufferTarget.CopyReadBuffer, nativeint offset, nativeint pixelSize, gc.AddrOfPinnedObject())
+                    GL.Check "could not read pixel from PBO"
+
+                    GL.BindBuffer(BufferTarget.CopyReadBuffer, 0)
+                    GL.Check "could not unbind PBO"
+                finally
+                    gc.Free()
+
+                match pixelType with
+                    | PixelType.UnsignedByte ->
+                        match pixelFormat with
+                            | PixelFormat.Rgba              -> C4b(data.[0], data.[1], data.[2], data.[3]).ToC4f()
+                            | PixelFormat.Bgra              -> C4b(data.[2], data.[1], data.[0], data.[3]).ToC4f()
+                            | PixelFormat.Rgb               -> C4b(data.[0], data.[1], data.[2], 255uy).ToC4f()
+                            | PixelFormat.Bgr               -> C4b(data.[2], data.[1], data.[0], 255uy).ToC4f()
+                            | PixelFormat.Luminance         -> C4b(data.[0], data.[0], data.[0], 255uy).ToC4f()
+                            | PixelFormat.Alpha             -> C4b(0uy, 0uy, 0uy, data.[0]).ToC4f()
+                            | PixelFormat.Red               -> C4b(data.[0], 0uy, 0uy, 255uy).ToC4f()
+                            | PixelFormat.Green             -> C4b(0uy, data.[0], 0uy, 255uy).ToC4f()
+                            | PixelFormat.Blue              -> C4b(0uy, 0uy, data.[0], 255uy).ToC4f()
+                            | PixelFormat.LuminanceAlpha    -> C4b(data.[0], data.[0], data.[0], data.[1]).ToC4f()
+                            | _                             -> failwithf "[GL] unsupported format %A" pixelFormat
+
+                    | PixelType.UnsignedShort ->
+                        let data = data.UnsafeCoerce<uint16>()
+                        match pixelFormat with
+                            | PixelFormat.Rgba              -> C4us(data.[0], data.[1], data.[2], data.[3]).ToC4f()
+                            | PixelFormat.Bgra              -> C4us(data.[2], data.[1], data.[0], data.[3]).ToC4f()
+                            | PixelFormat.Rgb               -> C4us(data.[0], data.[1], data.[2], 65535us).ToC4f()
+                            | PixelFormat.Bgr               -> C4us(data.[2], data.[1], data.[0], 65535us).ToC4f()
+                            | PixelFormat.Luminance         -> C4us(data.[0], data.[0], data.[0], 65535us).ToC4f()
+                            | PixelFormat.Alpha             -> C4us(0us, 0us, 0us, data.[0]).ToC4f()
+                            | PixelFormat.Red               -> C4us(data.[0], 0us, 0us, 65535us).ToC4f()
+                            | PixelFormat.Green             -> C4us(0us, data.[0], 0us, 65535us).ToC4f()
+                            | PixelFormat.Blue              -> C4us(0us, 0us, data.[0], 65535us).ToC4f()
+                            | PixelFormat.LuminanceAlpha    -> C4us(data.[0], data.[0], data.[0], data.[1]).ToC4f()
+                            | _                             -> failwithf "[GL] unsupported format %A" pixelFormat
+
+
+
+                    | _ ->
+                        failwithf "[GL] unsupported type %A" pixelType
+        )
+
+    member x.Dispose() =
+        use token = ctx.ResourceLock
+        ctx.Delete(ping)
+        ctx.Delete(pong)
+        if pbo <> 0 then
+            pbo <- 0
+            GL.DeleteBuffer(pbo)
+
+        pboSize <- 0n
+        currentFormat <- PixFormat(typeof<obj>, Col.Format.RGBA)
+        currentSize <- -V2i.II
+        textureFormat <- TextureFormat.Alpha
+        pixelType <- PixelType.UnsignedByte
+        pixelFormat <- PixelFormat.Alpha
+        channels <- 0
+        channelSize <- 0
+        mipMapLevels <- 0
+        bufferSize <- 0n
+
+
+
+    override x.Compute() =
+        pong :> ITexture
+
+    interface IDisposable with
+        member x.Dispose() = x.Dispose()
+
+    interface IStreamingTexture with
+        member x.Update(f,s,d) = x.Update(f,s,d)
+        member x.UpdateAsync(f,s,d) = x.UpdateAsync(f,s,d)
+        member x.ReadPixel(pos) = x.ReadPixel(pos)
+    
+
+
 
 [<AutoOpen>]
 module StreamingTextureExtensions =
@@ -195,22 +414,14 @@ module StreamingTextureExtensions =
     type Context with
         member x.CreateStreamingTexture(mipMaps : bool) =
             using x.ResourceLock (fun _ ->
-                let handle = GL.GenTexture()
-                GL.Check "could not create streaming texture"
-
-                StreamingTexture(x, handle, mipMaps)
+                new StreamingTexture(x, mipMaps)
             )
 
         member x.CreateStreamingTexture() =
             x.CreateStreamingTexture(false)
 
         member x.Delete(t : StreamingTexture) =
-            using x.ResourceLock (fun _ ->
-                t.Release()
-
-                GL.DeleteTexture(t.Handle)
-                GL.Check "could not delete streaming texture"
-            )
+            t.Dispose()
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module StreamingTexture =
