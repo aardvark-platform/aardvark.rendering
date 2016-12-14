ab3a1d3df9f4033eb7b510fab793d0afb88aec53
Harald Steinlechner <hs@vrvis.at>
2016-12-14 15:33:42 +0100

    Textures: added more default TextureParams constructions

--- src/Aardvark.Base.Rendering/Textures.fs
+++ src/Aardvark.Base.Rendering/Textures.fs
@@ -20,6 +20,8 @@ module TextureParams =
     
     let empty = { wantMipMaps = false; wantSrgb = false; wantCompressed = false}
     let compressed = { wantMipMaps = false; wantSrgb = false; wantCompressed = true }
+    let mipmapped = { wantMipMaps = false; wantSrgb = false; wantCompressed = true }
+    let mipmappedCompressed = { wantMipMaps = true; wantSrgb = false; wantCompressed = true }
 
 type BitmapTexture(bmp : System.Drawing.Bitmap, textureParams : TextureParams) =
     [<Obsolete("use texture params instead")>]
