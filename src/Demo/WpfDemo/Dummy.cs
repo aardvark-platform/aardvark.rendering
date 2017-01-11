7ce72afc7755c822b1bd101a21d4b38f6e8180e4
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-01-11 10:16:57 +0100

    [Demo] how complicated can it be?

---
+++ src/Demo/WpfDemo/Dummy.cs
@@ -0,0 +1,18 @@
+﻿#if __MonoCS__
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDemo
+{
+    public class Dummy
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
+#endif
