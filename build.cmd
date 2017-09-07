c7565d55496316449af98bc75a898460b672a4f6
walch <walch@vrvis.at>
2017-09-07 15:55:01 +0200

    update buildscript fixed broken linux-build

--- build.cmd
+++ build.cmd
@@ -8,12 +8,15 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
+if NOT exist paket.lock (
+    echo No paket.lock found, running paket install.
+    .paket\paket.exe install
+)
+
 .paket\paket.exe restore --group Build
 if errorlevel 1 (
   exit /b %errorlevel%
 )
-
-
 
 SET FSI_PATH=packages\build\FAKE\tools\Fake.exe
 "%FSI_PATH%" "build.fsx" Dummy --fsiargs build.fsx --shadowcopyreferences+ %* 
