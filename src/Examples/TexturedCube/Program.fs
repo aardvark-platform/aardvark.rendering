0ae23ba168e8e637caab3108597da5cdd2931011
krauthaufen <krauthaufen@awx.at>
2018-01-13 11:54:18 +0100

    [Examples] - Triangle Example - TexturedCube Example
    Former-commit-id: 073517d7ecdcb496f0c8a0cd544c245b5d783aa5

---
+++ src/Examples/TexturedCube/Program.fs
@@ -0,0 +1,47 @@
+﻿open Aardvark.Base
+open Aardvark.Base.Rendering
+open Aardvark.Base.Incremental
+open Aardvark.SceneGraph
+open Aardvark.Application
+
+[<EntryPoint>]
+let main argv = 
+    
+    // first we need to initialize Aardvark's core components
+    Ag.initialize()
+    Aardvark.Init()
+
+
+    // lets define the bounds/color for our box
+    // NOTE that the color is going to be ignored since we're using a texture
+    let box = Box3d(-V3d.III, V3d.III)
+    let color = C4b.Red
+
+    let sg = 
+        // thankfully aardvark defines a primitive box
+        Sg.box (Mod.constant color) (Mod.constant box)
+
+            // apply the texture as "DiffuseTexture"
+            |> Sg.diffuseTexture DefaultTextures.checkerboard
+
+            // apply a shader ...
+            // * transforming all vertices
+            // * looking up the DiffuseTexture 
+            // * applying a simple lighting to the geometry (headlight)
+            |> Sg.shader {
+                do! DefaultSurfaces.trafo
+                do! DefaultSurfaces.diffuseTexture
+                do! DefaultSurfaces.simpleLighting
+            }
+    
+
+    // show the scene in a simple window
+    show {
+        backend Backend.GL
+        display Display.Mono
+        debug false
+        samples 8
+        scene sg
+    }
+
+    0
