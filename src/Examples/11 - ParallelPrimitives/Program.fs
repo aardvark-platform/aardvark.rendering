5fc2f2f785780e1988fa68f1dbb87d09c49872d7
krauthaufen <krauthaufen@awx.at>
2018-01-14 17:58:32 +0100

    [Examples] improved parallel filter implementation
    Former-commit-id: 5fad4331ea11e626052e3201b9af94124cc9f14c

--- src/Examples/11 - ParallelPrimitives/Program.fs
+++ src/Examples/11 - ParallelPrimitives/Program.fs
@@ -13,14 +13,15 @@ module Shader =
     open FShade
 
     [<LocalSize(X = 64)>]
-    let resolve (n : int) (src : int[]) (bits : int[]) (targets : int[]) (result : int[]) =
+    let resolve (n : int) (src : int[]) (targets : int[]) (result : int[]) =
         compute {
             let id = getGlobalId().X
             if id < n then
                 let inputValue = src.[id]
-                let targetLocation = targets.[id]-1
-                if bits.[id] = 1 then
-                    result.[targetLocation] <- inputValue
+                let r = targets.[id]
+                let l = if id > 0 then targets.[id-1] else 0
+                if l < r then
+                    result.[r - 1] <- inputValue
         }
 
 let parallelFilter<'a when 'a : unmanaged> (runtime : IRuntime) (arr : 'a[]) (predicate : Expr<'a -> bool>) =
@@ -28,29 +29,33 @@ let parallelFilter<'a when 'a : unmanaged> (runtime : IRuntime) (arr : 'a[]) (pr
     let par = ParallelPrimitives(runtime)
     let input = runtime.CreateBuffer<'a>(arr)
     let bits = runtime.CreateBuffer<int>(input.Count)
-    let bitsum = runtime.CreateBuffer<int>(input.Count)
     let result = runtime.CreateBuffer<'a>(arr.Length)
 
     let mapper = par.CompileMap(<@ fun i e -> if (%predicate) e then 1 else 0 @>, input, bits)
-    let scanner = par.CompileScan(<@ (+) @>, bits, bitsum)
+    let scanner = par.CompileScan(<@ (+) @>, bits, bits)
 
     let resolveTargetShader = runtime.CreateComputeShader Shader.resolve
     let targetWrite = runtime.NewInputBinding resolveTargetShader
     targetWrite.["src"] <- input
     targetWrite.["n"] <- input.Count
-    targetWrite.["bits"] <- bits
-    targetWrite.["targets"] <- bitsum
+    targetWrite.["targets"] <- bits
     targetWrite.["result"] <- result
     targetWrite.Flush()
     let ceilDiv (v : int) (d : int) =
         if v % d = 0 then v / d
         else 1 + v / d
 
+    let bitsCPU = Array.zeroCreate arr.Length
+    let bitsumCPU = Array.zeroCreate arr.Length
+
     let commands = [
         ComputeCommand.Execute mapper
         ComputeCommand.Sync(bits.Buffer,BufferAccess.ShaderWrite,BufferAccess.ShaderRead)
+        ComputeCommand.Copy(bits, bitsCPU)
+
         ComputeCommand.Execute scanner
-        ComputeCommand.Sync(bitsum.Buffer,BufferAccess.ShaderWrite,BufferAccess.ShaderRead)
+        ComputeCommand.Sync(bits.Buffer,BufferAccess.ShaderWrite,BufferAccess.ShaderRead)
+        ComputeCommand.Copy(bits, bitsumCPU)
         ComputeCommand.Bind(resolveTargetShader)
         ComputeCommand.SetInput targetWrite
         ComputeCommand.Dispatch (ceilDiv (int input.Count) 64)
@@ -59,13 +64,11 @@ let parallelFilter<'a when 'a : unmanaged> (runtime : IRuntime) (arr : 'a[]) (pr
     let program = runtime.Compile commands
     program.Run()
 
-    let bitsCPU =  bits.Download()
-    printfn "bits: %A" bitsCPU
-    let bitsumCPU = bitsum.Download()
-    printfn "bitsum: %A" bitsumCPU
+    Log.line "bits: %A" bitsCPU
+    Log.line "bitsum: %A" bitsumCPU
     let max = bitsumCPU.[bitsumCPU.Length-1]
     let result2 = result.[0..max-1].Download()
-    printfn "result: %A" result2
+    Log.line "result: %A" result2
     ()
 
 [<EntryPoint>]
