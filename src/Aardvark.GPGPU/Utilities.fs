b2beb20ac169b185380500b6dde69514aa84a224
krauthaufen <krauthaufen@awx.at>
2018-02-14 14:34:56 +0100

    [GPGPU] segmentation

---
+++ src/Aardvark.GPGPU/Utilities.fs
@@ -0,0 +1,16 @@
+﻿namespace Aardvark.Base
+
+
+[<AutoOpen>]
+module GPGPUHelpers =
+            
+    let inline ceilDiv (v : ^a) (a : ^a) : ^a =
+        if v % a = LanguagePrimitives.GenericZero then v / a
+        else LanguagePrimitives.GenericOne + v / a
+
+
+    let ceilDiv2 (v : V2i) (a : V2i) =
+        V2i(ceilDiv v.X a.X, ceilDiv v.Y a.Y)
+
+    let ceilDiv3 (v : V3i) (a : V3i) =
+        V3i(ceilDiv v.X a.X, ceilDiv v.Y a.Y, ceilDiv v.Z a.Z)
