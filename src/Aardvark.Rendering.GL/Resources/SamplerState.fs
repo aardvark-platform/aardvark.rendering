b8ec71e6f2585d19c53afc96d59d1c4fe18871b7
krauthaufen <krauthaufen@awx.at>
2018-04-09 16:26:07 +0200

    [GL] removed useless OpenTK packages
    Former-commit-id: 8c84005997c64f46e16653467b0906b296a6856e

--- src/Aardvark.Rendering.GL/Resources/SamplerState.fs
+++ src/Aardvark.Rendering.GL/Resources/SamplerState.fs
@@ -131,8 +131,10 @@ module SamplerExtensions =
         GL.SamplerParameter(handle, SamplerParameterName.TextureMagFilter, magFilter d.Filter.Mag)
         GL.Check "could not set MagFilter for sampler"
 
+        
+
         if d.ComparisonFunction <> SamplerComparisonFunction.None then
-            GL.SamplerParameter(handle, SamplerParameterName.TextureCompareMode, OpenTK.Graphics.TextureCompareMode.CompareRefToTexture |> int)
+            GL.SamplerParameter(handle, SamplerParameterName.TextureCompareMode, OpenTK.Graphics.OpenGL4.TextureCompareMode.CompareRefToTexture |> int)
             GL.Check "could not set comparison mode for sampler"
 
             GL.SamplerParameter(handle, SamplerParameterName.TextureCompareFunc, compareFunc d.ComparisonFunction)
