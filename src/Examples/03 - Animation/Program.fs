d67ea8d64343d39bee55f4e9a806c4f3fc2b1d36
krauthaufen <krauthaufen@awx.at>
2018-04-11 16:08:26 +0200

    bla
    Former-commit-id: 20d79dd402bc71929098f23189cc2d6ada40216a

--- src/Examples/03 - Animation/Program.fs
+++ src/Examples/03 - Animation/Program.fs
@@ -15,7 +15,7 @@ let main argv =
     // and may show it later.
     let win =
         window {
-            backends [Backend.GL; Backend.Vulkan]
+            backend Backend.GL
             display Display.Mono
             debug true
             samples 8
@@ -24,26 +24,42 @@ let main argv =
     // define a dynamic transformation depending on the window's time
     // This time is a special value that can be used for animations which
     // will be evaluated when rendering the scene
-    let dynamicTrafo =
-        let startTime = System.DateTime.Now
-        win.Time |> Mod.map (fun t ->
-            let t = (t - startTime).TotalSeconds
-            Trafo3d.RotationZ (0.5 * t)
-        )
+
+    let sg = 
+        let rand = RandomSystem()
+        Sg.ofList [
+            for x in -10 .. 10 do
+                for y in -10 .. 10 do
+                    let rand = rand.UniformDouble()
+                    let dynamicTrafo =
+                        let startTime = System.DateTime.Now
+                        win.Time |> Mod.map (fun t ->
+                            let t = (t - startTime).TotalSeconds
+                            Trafo3d.RotationZ (0.5 * t + rand) * Trafo3d.Translation(V3d(float x, float y, 0.0))
+                        )
+
+                    let box = Box3d(-V3d.III * 0.2, V3d.III * 0.2)
+                    let color = C4b.Red
+
+                    let sg = 
+                        // create a red box with a simple shader
+                        Sg.box (Mod.constant color) (Mod.constant box)
+                        // apply the dynamic transformation to the box
+                        |> Sg.trafo dynamicTrafo
+
+                    yield sg
+        ]
+
 
     let box = Box3d(-V3d.III, V3d.III)
     let color = C4b.Red
 
     let sg = 
-        // create a red box with a simple shader
-        Sg.box (Mod.constant color) (Mod.constant box)
+        sg
             |> Sg.shader {
                 do! DefaultSurfaces.trafo
                 do! DefaultSurfaces.simpleLighting
             }
-
-            // apply the dynamic transformation to the box
-            |> Sg.trafo dynamicTrafo
     
     // show the window
     win.Scene <- sg
