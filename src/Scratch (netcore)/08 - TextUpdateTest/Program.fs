0b36bd7c5b06ae9fc3af56f634dea115484600aa
Harald Steinlechner <haraldsteinlechner@gmail.com>
2019-08-16 11:12:58 +0200

    added TextUpdate scratch

---
+++ src/Scratch (netcore)/08 - TextUpdateTest/Program.fs
@@ -0,0 +1,150 @@
+﻿open Aardvark.Base
+open Aardvark.Base.Rendering
+open Aardvark.Base.Incremental
+open Aardvark.SceneGraph
+open Aardvark.Rendering.Text
+open Aardvark.Application
+open System.Threading
+
+open Aardvark.Base.Ag
+// This example illustrates how to render a simple triangle using aardvark.
+
+module IncrementalExtensions = 
+    open System.Threading
+
+    let throttled (name : string) (ms : int) (f : 'a -> unit) (m : IMod<'a>) : IMod<'a> = 
+        let v = MVar.empty()
+        let r = Mod.init (Mod.force m)
+        let f () = 
+            while true do
+                let () = MVar.take v
+                let v = Mod.force m
+                f v 
+                transact (fun _ -> r.Value <- v)
+                Thread.Sleep ms
+
+        let d = m.AddMarkingCallback(fun _ -> MVar.put v ())
+
+        let t = Thread(ThreadStart f)
+        t.IsBackground <- true
+        t.Name <- name
+        t.Start()
+        r :> IMod<_>
+
+
+type WtfRuntimeApplicator(r : IRuntime, child : ISg) =
+    inherit Sg.AbstractApplicator(child)
+    member x.Runtime = r
+
+[<Semantic>]
+type RuntimeSem() =
+    member x.Runtime(w : WtfRuntimeApplicator) = w.Child?Runtime <- w.Runtime
+
+module Sg = 
+    let applyRuntime (r : IRuntime) (sg : ISg) = WtfRuntimeApplicator(r,sg) :> ISg
+
+[<EntryPoint>]
+let main argv = 
+    
+    Ag.initialize()
+    Aardvark.Init()
+
+    // window { ... } is similar to show { ... } but instead
+    // of directly showing the window we get the window-instance
+    // and may show it later.
+    let win =
+        window {
+            backend Backend.Vulkan
+            display Display.OpenVR
+            debug true
+            samples 1
+        }
+
+    let runtime = win.Runtime
+
+    let box = Box3d(-V3d.III, V3d.III)
+    let color = C4b.Red
+
+
+    let cfg = { Aardvark.Rendering.Text.TextConfig.Default with color = C4b.Red; align = TextAlignment.Center }
+    let inputText = Mod.init "aaaa"
+
+    let text = inputText |> Mod.map (fun t -> cfg.Layout t)
+
+
+    let size = V2i(512,512)
+    let color = runtime.CreateTexture(size, TextureFormat.Rgba8, 1, 1)
+    let prepare = 
+
+        let depth = runtime.CreateRenderbuffer(size, RenderbufferFormat.Depth24Stencil8, 1)
+
+        let signature =
+            runtime.CreateFramebufferSignature [
+                DefaultSemantic.Colors, { format = RenderbufferFormat.Rgba8; samples = 1 }
+                DefaultSemantic.Depth, { format = RenderbufferFormat.Depth24Stencil8; samples = 1 }
+            ]
+
+        let fbo = 
+            runtime.CreateFramebuffer(
+                signature, 
+                Map.ofList [
+                    DefaultSemantic.Colors, ({ texture = color; slice = 0; level = 0 } :> IFramebufferOutput)
+                    DefaultSemantic.Depth, (depth :> IFramebufferOutput)
+                ]
+            )
+        let mutable old : Option<IRenderTask> = None
+        let t = Mod.init (text |> Mod.force) |> Sg.shape |> Sg.applyRuntime runtime |> Sg.compile runtime signature
+        fun (s : ShapeList) ->
+            let o = old
+            Log.startTimed "compile text"
+            let s = Sg.shape (Mod.constant s) 
+            s?Runtime <- runtime
+            let task = 
+                s 
+                |> Sg.applyRuntime runtime 
+                |> Sg.scale 0.1
+                |> Sg.viewTrafo (Mod.constant Trafo3d.Identity)
+                |> Sg.projTrafo (Frustum.ortho (Box3d.Unit) |> Frustum.projTrafo |> Mod.constant)
+                |> Sg.compile runtime signature
+            old <- Some task
+            task.Run(RenderToken.Empty, fbo)
+            match o with
+                | None -> ()
+                | Some o -> o.Dispose()
+            Log.stop()
+
+    let text = IncrementalExtensions.throttled "ttext" 400000 prepare text
+    let overlay = text |> Sg.shape // |> Sg.scale 0.02
+
+    let changeThread =
+        let f () =
+            while true do
+                Thread.Sleep 1
+                transact (fun _ -> inputText.Value <- System.Guid.NewGuid() |> string)
+        let t = Thread(ThreadStart f)
+        t.IsBackground <- true
+        t.Start()
+        t
+    
+    let sw = System.Diagnostics.Stopwatch.StartNew()
+    let t = win.Time |> Mod.map (fun _ -> Trafo3d.RotationZ (sw.Elapsed.TotalSeconds * 0.1))
+
+    let sg = 
+        // create a red box with a simple shader
+        //Sg.box (Mod.constant color) (Mod.constant box)
+            Sg.fullScreenQuad
+            |> Sg.diffuseTexture (color :> ITexture |> Mod.constant)
+            //overlay
+            |> Sg.trafo t
+            |> Sg.shader {
+                do! DefaultSurfaces.trafo
+                do! DefaultSurfaces.constantColor C4f.Red
+                do! DefaultSurfaces.diffuseTexture
+            }
+
+    
+    // show the window
+    win.Scene <- sg
+    win.Run()
+
+    0
