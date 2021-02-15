29c7d7c5b6c1f438bb4c53da7131977d5bcc691f
Christian Luksch <luksch@vrvis.at>
2021-02-15 23:04:14 +0100

    avoid alloc of empty arrays;

--- src/Aardvark.Rendering/Utilities/Locks/RenderTaskLock.fs
+++ src/Aardvark.Rendering/Utilities/Locks/RenderTaskLock.fs
@@ -7,10 +7,10 @@ type RenderTaskLock() =
     let mutable lockedResources : CountingHashSet<ILockedResource> = CountingHashSet.empty
 
     member x.Run f =
-        let res = lock lockObj (fun () -> CountingHashSet.toArray lockedResources)
-        for l in res do l.Lock.Enter(ResourceUsage.Render, l.OnLock)
+        let res = lock lockObj (fun () -> if lockedResources.IsEmpty then null else CountingHashSet.toArray lockedResources)
+        if res <> null then for l in res do l.Lock.Enter(ResourceUsage.Render, l.OnLock)
         try f()
-        finally for l in res do l.Lock.Exit(l.OnUnlock)
+        finally if res <> null then for l in res do l.Lock.Exit(l.OnUnlock)
 
     member x.Add(r : ILockedResource) =
         lock lockObj (fun () -> lockedResources <- lockedResources.Add r)
