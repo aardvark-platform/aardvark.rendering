8d6bf5fc4c30e4157e103898482bf6f5ca16f48a
Harald Steinlechner <haraldsteinlechner@gmail.com>
2021-04-13 17:40:33 +0200

    added UniformUpdate benchmark

---
+++ src/Scratch (netcore)/18 -/Program.fs
@@ -0,0 +1,41 @@
+﻿open Aardvark.Base
+open Aardvark.Rendering
+open FSharp.Data.Adaptive
+open Aardvark.SceneGraph
+open Aardvark.Application
+
+// This example illustrates how to render a simple triangle using aardvark.
+
+[<EntryPoint>]
+let main argv = 
+    
+    
+    Aardvark.Init()
+
+    // window { ... } is similar to show { ... } but instead
+    // of directly showing the window we get the window-instance
+    // and may show it later.
+    let win =
+        window {
+            backend Backend.Vulkan
+            display Display.Mono
+            debug true
+            samples 8
+        }
+
+    let box = Box3d(-V3d.III, V3d.III)
+    let color = C4b.Red
+
+    let sg = 
+        // create a red box with a simple shader
+        Sg.box (AVal.constant color) (AVal.constant box)
+            |> Sg.shader {
+                do! DefaultSurfaces.trafo
+                do! DefaultSurfaces.simpleLighting
+            }
+    
+    // show the window
+    win.Scene <- sg
+    win.Run()
+
+    0
