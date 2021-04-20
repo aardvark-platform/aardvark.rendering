00ad6ab8db5bb4fec15cda8eb7a61bd57fd343a9
Harald Steinlechner <haraldsteinlechner@gmail.com>
2021-04-20 14:54:35 +0200

    point example upgrade @ElitzVasileva

--- src/Examples (netcore)/35 - Points/Program.fs
+++ src/Examples (netcore)/35 - Points/Program.fs
@@ -19,22 +19,78 @@ module Shader =
             pos : V4d
             [<PointCoord>]
             tc : V2d
-            [<PointSize>]
-            ps : float
+            [<Semantic("DepthRange"); Interpolation(InterpolationMode.Flat)>] 
+            depthRange : float
+            [<FragCoord>] 
+            fc : V4d
+            [<Semantic("PointPixelSize")>] ps : float
+            [<PointSize>] s: float
+        }
+
+    type Fragment = 
+        {   
+            [<Color>]
+            color : V4d
+
+            [<Depth(DepthWriteMode.OnlyGreater)>] 
+            d : float
         }
 
     let pointSprite (v : Vertex) = 
         vertex {
-            return { v with ps = 30.0 }
+            let mv = uniform.ModelViewTrafo
+            let vp = mv * v.pos
+
+            let s = uniform.PointSize / V2d uniform.ViewportSize
+            let opp = uniform.ProjTrafo * vp
+            let pp0 = opp.XYZ / opp.W
+
+
+            let vpx = 
+                let temp = uniform.ProjTrafoInv * V4d(pp0 + V3d(s.X, 0.0, 0.0), 1.0)
+                temp.XYZ / temp.W
+     
+            let vpy = 
+                let temp = uniform.ProjTrafoInv * V4d(pp0 + V3d(0.0, s.Y, 0.0), 1.0)
+                temp.XYZ / temp.W
+
+            let worldRadius =
+                0.5 * (Vec.length (vp.XYZ - vpx) + Vec.length (vp.XYZ - vpy))
+
+            let vpz = vp + V4d(0.0, 0.0, worldRadius, 0.0)
+            let ppz = uniform.ProjTrafo * vpz
+            let ppz = ppz.XYZ / ppz.W
+
+            let depthRange = abs (ppz.Z - pp0.Z)
+
+            let pixelDist = 
+                uniform.PointSize
+ 
+            let pixelDist = 
+                if ppz.Z < -1.0 then -1.0
+                else pixelDist
+
+            return { v with ps = floor pixelDist; s = pixelDist; pos = V4d(ppz, 1.0); depthRange = depthRange; }
         }
 
 
     let pointSpriteFragment (v : Vertex) = 
         fragment {
-            let c = v.tc * 2.0 - V2d.II
-            let f = Vec.dot c c
-            let diffuse = sqrt (max 0.0 (1.0 - f))
-            return V4d(V3d.III * diffuse, 1.0)
+           let mutable cc = v.tc
+           let c = v.tc * 2.0 - V2d.II
+           let f = Vec.dot c c - 1.0
+           if f > 0.0 then discard()
+   
+           let t = 1.0 - sqrt (-f)
+           let depth = v.fc.Z
+           let outDepth = depth + v.depthRange * t
+
+           let c = c * (1.0 + 2.0 / v.ps)
+           let f = Vec.dot c c 
+           let diffuse = sqrt (max 0.0 (1.0 - f))
+           let color = V3d.III * diffuse
+
+           return { color = V4d(color, 1.0); d = outDepth }
         }
 
 
@@ -52,10 +108,10 @@ let main argv =
         Sg.draw IndexedGeometryMode.PointList
         |> Sg.vertexArray DefaultSemantic.Positions pts
         |> Sg.shader {
-            do! DefaultSurfaces.trafo
             do! Shader.pointSprite
             do! Shader.pointSpriteFragment
         }
+        |> Sg.uniform "PointSize" (AVal.constant 50.0)
     
 
     // show the scene in a simple window
