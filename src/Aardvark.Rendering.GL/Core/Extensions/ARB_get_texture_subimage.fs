b96dd00993a3d46a35e8f9f33d9630e846a403fb
Martin <mautner@vrvis.at>
2021-09-30 15:35:59 +0200

    [GL] Fix check for GL_ARB_get_texture_sub_image

--- src/Aardvark.Rendering.GL/Core/Extensions/ARB_get_texture_subimage.fs
+++ src/Aardvark.Rendering.GL/Core/Extensions/ARB_get_texture_subimage.fs
@@ -3,6 +3,7 @@
 open System
 open OpenTK.Graphics
 open OpenTK.Graphics.OpenGL4
+open Aardvark.Base
 open Aardvark.Rendering.GL
 
 [<AutoOpen>]
@@ -16,17 +17,44 @@ module ARB_get_texture_subimage =
 
     type GL private() =
 
-        static let supported = ExtensionHelpers.isSupported (Version(4,5)) "GL_ARB_get_texture_subimage"
+        static let supported = ExtensionHelpers.isSupported (Version(4,5)) "GL_ARB_get_texture_sub_image"
 
         static member ARB_get_texture_subimage = supported
 
         static member GetTextureSubImage(texture : int,
                                          level : int, xoffset : int, yoffset : int, zoffset : int,
                                          width : int, height : int, depth : int, format : PixelFormat,
-                                         pixelType : PixelType, bufferSize : int, pixels : nativeint) =
+                                         typ : PixelType, bufferSize : int, pixels : nativeint) =
             if supported then
                 GL.Arb.GetTextureSubImage(texture, level, xoffset, yoffset, zoffset,
                                           width, height, depth,
-                                          format, pixelType, bufferSize, pixels)
+                                          format, typ, bufferSize, pixels)
             else
                 failwith "glGetTextureSubImage is not available!"
+
+        static member GetTextureSubImage(texture : int,
+                                         level : int, offset : V3i, size : V3i,
+                                         format : PixelFormat, typ : PixelType,
+                                         bufferSize : int, pixels : nativeint) =
+            GL.GetTextureSubImage(texture, level,
+                                  offset.X, offset.Y, offset.Z,
+                                  size.X, size.Y, size.Z,
+                                  format, typ, bufferSize, pixels)
+
+        static member GetCompressedTextureSubImage(texture : int, level : int,
+                                                   xoffset : int, yoffset : int, zoffset : int,
+                                                   width : int, height : int, depth : int,
+                                                   bufferSize : int, pixels : nativeint) =
+            if supported then
+                GL.Arb.GetCompressedTextureSubImage(texture, level, xoffset, yoffset, zoffset,
+                                                    width, height, depth,
+                                                    bufferSize, pixels)
+            else
+                failwith "glGetCompressedTextureSubImage is not available!"
+
+        static member GetCompressedTextureSubImage(texture : int, level : int, offset : V3i, size : V3i,
+                                                   bufferSize : int, pixels : nativeint) =
+            GL.GetCompressedTextureSubImage(texture, level,
+                                            offset.X, offset.Y, offset.Z,
+                                            size.X, size.Y, size.Z,
+                                            bufferSize, pixels)
