d26beca1545187a6ad0946d8e4487379d3cf644f
Christian Luksch <luksch@vrvis.at>
2022-07-13 19:21:14 +0200

    fixed logLines

--- src/Aardvark.Rendering/Utilities/ShaderCodeReporting.fs
+++ src/Aardvark.Rendering/Utilities/ShaderCodeReporting.fs
@@ -16,16 +16,15 @@ module ShaderCodeReporting =
         let mutable lineStart = 0
         let mutable lineCnt = 1
         while (lineEnd >= 0) do
-            let line = code.Substring(lineStart, lineEnd - lineStart + 1)
             sb.Append(lineCnt.ToString().PadLeft(lineColumns)) |> ignore
             sb.Append(": ")  |> ignore
-            sb.Append(line) |> ignore
+            sb.Append(code, lineStart, lineEnd - lineStart + 1) |> ignore
             lineStart <- lineEnd + 1
             lineCnt <- lineCnt + 1
             lineEnd <- code.IndexOf('\n', lineStart)
             ()
 
-        let lastLine = code.Substring(lineStart)
+        let lastLine = code.Substring(lineStart) // last line might not end with '\n'
         if lastLine.Length > 0 then
             sb.Append(lineCnt.ToString()) |> ignore
             sb.Append(": ")  |> ignore
@@ -37,7 +36,7 @@ module ShaderCodeReporting =
         let lineCount = String.lineCount code
         let lineColumns = 1 + int (Fun.Log10 lineCount)
         let lineFormatLen = lineColumns + 3
-        let sb = new System.Text.StringBuilder(code.Length + lineFormatLen * lineCount + 10)
+        let sb = new System.Text.StringBuilder(256) // estimated max line length
             
         let fmtStr = "{0:" + lineColumns.ToString() + "} : "
         let mutable lineEnd = code.IndexOf('\n')
@@ -45,20 +44,19 @@ module ShaderCodeReporting =
         let mutable lineCnt = 1
         while (lineEnd >= 0) do
             sb.Clear() |> ignore
-            let line = code.Substring(lineStart, lineEnd - lineStart)
             sb.Append(lineCnt.ToString().PadLeft(lineColumns)) |> ignore
             sb.Append(": ")  |> ignore
-            sb.Append(line) |> ignore
-            Report.Text("{0}", sb.ToString())
+            sb.Append(code, lineStart, lineEnd - lineStart - 1) |> ignore // assume line ends with \r\n -> Report.Line needs this removed, on the other hand Report.Text does not properly output log file (only looking right in console, for some reason "withLineNumbers" + Report.Line works)
+            Report.Line(sb.ToString())
             lineStart <- lineEnd + 1
             lineCnt <- lineCnt + 1
             lineEnd <- code.IndexOf('\n', lineStart)
             ()
 
-        let lastLine = code.Substring(lineStart)
+        let lastLine = code.Substring(lineStart) // last line might not end with '\n'
         if lastLine.Length > 0 then
             sb.Clear() |> ignore
             sb.Append(lineCnt.ToString()) |> ignore
             sb.Append(": ")  |> ignore
             sb.Append(lastLine) |> ignore
-            Report.Line("{0}", sb.ToString())
+            Report.Line(sb.ToString())
