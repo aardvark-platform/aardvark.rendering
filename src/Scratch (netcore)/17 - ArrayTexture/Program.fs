c57b2759026dab3ea6dfd58e4c61d6fa67d96647
Martin <mautner@vrvis.at>
2022-08-04 10:00:58 +0200

    Fix and extend texture array example

--- src/Scratch (netcore)/17 - ArrayTexture/Program.fs
+++ src/Scratch (netcore)/17 - ArrayTexture/Program.fs
@@ -6,40 +6,67 @@ open Aardvark.SceneGraph
 open Aardvark.Application
 open FShade
 
-module Shady =
-    let sam =
-        sampler2dArray {
-            texture uniform?BlaBlub
+module Semantic =
+    let DiffuseColorTextureSlice = Sym.ofString "DiffuseColorTextureSlice"
+
+module Shader =
+
+    let MaxTextureSlices = 32
+
+    type UniformScope with
+        member x.DiffuseColorTextureSlice : int = x?DiffuseColorTextureSlice
+
+    let private diffuseSamplerArray =
+        sampler2d {
+            textureArray uniform?DiffuseColorTexture MaxTextureSlices
             filter Filter.MinMagLinear
             addressU WrapMode.Clamp
             addressV WrapMode.Clamp
         }
 
-    let frag (v : Effects.Vertex) =
-        fragment {
-            let slice : int = uniform?choice
-            return sam.Sample(v.tc,slice)
+    let private diffuseSampler =
+        sampler2dArray {
+            texture uniform?DiffuseColorTexture
+            filter Filter.MinMagLinear
+            addressU WrapMode.Clamp
+            addressV WrapMode.Clamp
         }
 
-type Texy(runtime : ITextureRuntime, size : V2i, count : aval<int>) =
+    let diffuseTextureArray (v : Effects.Vertex) =
+        fragment {
+            let slice = uniform.DiffuseColorTextureSlice
+            return diffuseSampler.Sample(v.tc, slice)
+        }
+
+    let diffuseArrayOfTextures (v : Effects.Vertex) =
+        fragment {
+            let slice = uniform.DiffuseColorTextureSlice
+            return diffuseSamplerArray.[slice].Sample(v.tc)
+        }
+
+module Texture =
+
+    let private rand = RandomSystem()
+
+    let generatePix (size : V2i) =
+        let pi = PixImage<float32>(Col.Format.RGBA, size)
+        let data = pi.GetMatrix<C4f>()
+        data.SetByCoord(ignore >> rand.UniformC3f().ToC4f) |> ignore
+        pi
+
+    let generate (size : V2i) =
+        PixTexture2d(generatePix size, false) :> ITexture
+
+type AdaptiveTexture2DArray(runtime : ITextureRuntime, size : V2i, count : aval<int>) =
     inherit AdaptiveResource<IBackendTexture>()
 
     let mutable handle : Option<IBackendTexture * int> = None
-
-    let rand = RandomSystem()
-
-    let generateTexture() =
-        let pimg = PixImage<float32>(Col.Format.RGBA, size)
-        let mat = pimg.GetMatrix<C4f>()
-        let b() = rand.UniformC3f().ToC4f()
-        mat.SetByCoord(fun _ -> b()) |> ignore
-        pimg
 
     let createAndUploadTexture (count : int) =
         Log.warn "Create texture"
         let t = runtime.CreateTexture2DArray(size, TextureFormat.Rgba32f, 1, 1, count)
         for slice in 0.. count - 1 do
-            let tex = generateTexture()
+            let tex = Texture.generatePix size
             t.Upload(tex, 0, slice)
 
         handle <- Some (t, count)
@@ -82,7 +109,7 @@ let main argv =
         window {
             backend Backend.GL
             display Display.Mono
-            debug false
+            debug true
             samples 8
         }
 
@@ -94,42 +121,66 @@ let main argv =
                 SymDict.ofList [
                     DefaultSemantic.Positions, [| V3f.OOO; V3f.IOO; V3f.IIO; V3f.OIO |] :> Array
                     DefaultSemantic.Colors, [| C4b.Red; C4b.Green; C4b.Blue; C4b.Yellow |] :> Array
-                    DefaultSemantic.DiffuseColorCoordinates, [|V2d.OO; V2d.IO; V2d.II; V2d.OI|] :> Array
+                    DefaultSemantic.DiffuseColorCoordinates, [|V2f.OO; V2f.IO; V2f.II; V2f.OI|] :> Array
                 ]
         )
 
     let rand = RandomSystem()
-    // GL_MAX_ARRAY_TEXTURE_LAYERS 2048
-    // GL_MAX_3D_TEXTURE_SIZE 16384
-    let rt = win.Runtime
+    let runtime = win.Runtime
 
+    let size = V2i 64
     let slice = AVal.init 0
-
-    let count = AVal.init 10
+    let count = AVal.init 12
     let onoff = AVal.init true
 
-    let texy = Texy(rt, V2i(64), count)
+    // Setting this to true results in an array of samplers with
+    // individual textures to be created. These do need to be the same size
+    // as is the case with a real texture array. However, the number of textures
+    // must be known in advance, and all elements must be set.
+    let useArrayOfSamplers = false
+
+    let applyTexture =
+        if useArrayOfSamplers then
+            let textures =
+                count |> AVal.map (fun count ->
+                    Array.init Shader.MaxTextureSlices (fun i ->
+                        if i < count then
+                            Texture.generate size
+                        else
+                            NullTexture() :> ITexture
+                    )
+                )
+
+            Sg.uniform DefaultSemantic.DiffuseColorTexture textures
+
+        else
+            let texture = AdaptiveTexture2DArray(runtime, size, count)
+            Sg.diffuseTexture texture
 
     let sg =
         onoff |> AVal.map (fun o ->
             if not o then Sg.empty
             else
                 quadGeometry
-                    |> Sg.ofIndexedGeometry
-                    |> Sg.effect [
-                        DefaultSurfaces.trafo |> toEffect
-                        DefaultSurfaces.vertexColor |> toEffect
-                        Shady.frag |> toEffect
-                       ]
-                    |> Sg.texture (Sym.ofString "BlaBlub") texy
-                    |> Sg.uniform "choice" slice
+                |> Sg.ofIndexedGeometry
+                |> Sg.effect [
+                    DefaultSurfaces.trafo |> toEffect
+                    DefaultSurfaces.vertexColor |> toEffect
+
+                    if useArrayOfSamplers then
+                        Shader.diffuseArrayOfTextures |> toEffect
+                    else
+                        Shader.diffuseTextureArray |> toEffect
+                ]
+                |> Sg.uniform Semantic.DiffuseColorTextureSlice  slice
+                |> applyTexture
         ) |> Sg.dynamic
 
     win.Keyboard.DownWithRepeats.Values.Add (fun k ->
         match k with
         | Keys.OemPlus ->  transact(fun _ -> let nv = min (slice.Value + 1) (count.Value-1) in Log.line "slice %d (%d)" nv (count.Value-1); slice.Value <- nv)
         | Keys.OemMinus -> transact(fun _ -> let nv = max (slice.Value - 1) 0 in Log.line "slice %d (%d)" nv (count.Value-1); slice.Value <- nv)
-        | Keys.Space -> transact(fun _ -> let nv = rand.UniformInt(10)+1 in Log.line "new count: %d" nv; count.Value <- nv; slice.Value <- 0)
+        | Keys.Space -> transact(fun _ -> let nv = rand.UniformInt(Shader.MaxTextureSlices - 1) + 1 in Log.line "new count: %d" nv; count.Value <- nv; slice.Value <- 0)
         | Keys.Enter -> transact(fun _ -> onoff.Value <- not onoff.Value)
         | _ -> ()
     )
