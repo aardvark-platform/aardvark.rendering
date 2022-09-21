b0f21958b079b8e22a1bba541b694d735a97805a
Martin <mautner@vrvis.at>
2022-09-21 11:43:50 +0200

    [Tests] Add more tests for integer samplers

--- src/Tests/Aardvark.Rendering.Tests/Tests/Rendering/Samplers.fs
+++ src/Tests/Aardvark.Rendering.Tests/Tests/Rendering/Samplers.fs
@@ -33,19 +33,22 @@ module Samplers =
 
     module Cases =
 
-        let sample2Drgba32ui (runtime : IRuntime) =
+        let private sample2DSignedInteger (getPixImage : V2i -> PixImage<'T>) (format : TextureFormat) (samples : int) (runtime : IRuntime) =
             let size = V2i(256)
 
             use signature =
                 runtime.CreateFramebufferSignature([
-                    DefaultSemantic.Colors, TextureFormat.Rgba32ui
-                ])
+                    DefaultSemantic.Colors, format
+                ], samples = samples)
 
-            let input = PixImage.random32ui size
+            let input = getPixImage size
+
+            use inputTexture = runtime.CreateTexture2D(size, format)
+            inputTexture.Upload input
 
             use task =
                 Sg.fullScreenQuad
-                |> Sg.diffuseTexture' (PixTexture2d(input, false))
+                |> Sg.diffuseTexture' inputTexture
                 |> Sg.shader {
                     do! Shader.diffuseTexture
                 }
@@ -55,7 +58,7 @@ module Samplers =
             buffer.Acquire()
 
             try
-                let output = buffer.GetValue().Download().AsPixImage<uint32>()
+                let output = buffer.GetValue().Download().AsPixImage<'T>()
 
                 Expect.equal output.Size size "Unexpected texture size"
                 PixImage.compare V2i.Zero input output
@@ -63,8 +66,34 @@ module Samplers =
             finally
                 buffer.Release()
 
+        let sample2Drgba8i (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random8i TextureFormat.Rgba8i 1
+
+        let sample2Drgba16i (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random16i TextureFormat.Rgba16i 1
+
+        let sample2Drgba32i (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random32i TextureFormat.Rgba32i 1
+
+        // TODO: Use unsigned integer samplers once FShade supports them
+        let sample2Drgba8ui (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random8ui TextureFormat.Rgba8ui 1
+
+        let sample2Drgba16ui (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random16ui TextureFormat.Rgba16ui 1
+
+        let sample2Drgba32ui (samples : int) (runtime : IRuntime) =
+            runtime |> sample2DSignedInteger PixImage.random32ui TextureFormat.Rgba32ui 1
+
     let tests (backend : Backend) =
         [
-            "2D rgba32ui", Cases.sample2Drgba32ui
+            "2D rgba8i", Cases.sample2Drgba8i 1
+            "2D rgba16i", Cases.sample2Drgba16i 1
+            "2D rgba32i", Cases.sample2Drgba32i 1
+            "2D rgba32i multisampled", Cases.sample2Drgba32i 4
+
+            "2D rgba8ui", Cases.sample2Drgba8ui 1
+            "2D rgba16ui", Cases.sample2Drgba16ui 1
+            "2D rgba32ui", Cases.sample2Drgba32ui 1
         ]
         |> prepareCases backend "Samplers"
