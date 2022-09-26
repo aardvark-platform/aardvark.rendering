6311f906713a70c8c000e2cbeb85b0e8f7cc76c1
Martin <mautner@vrvis.at>
2022-09-26 15:55:55 +0200

    [Sg] Add onActivation

---
+++ src/Aardvark.SceneGraph/Semantics/Activate.fs
@@ -0,0 +1,25 @@
+﻿namespace Aardvark.SceneGraph.Semantics
+
+open Aardvark.Base
+open Aardvark.Base.Ag
+open Aardvark.SceneGraph
+
+open System
+
+[<AutoOpen>]
+module ActivateSemantics =
+
+    type Ag.Scope with
+        member x.Activate : list<unit -> IDisposable> = x?Activate
+
+    module Semantic =
+        let activate (s : Ag.Scope) : list<unit -> IDisposable> = s?Activate
+
+    [<Rule>]
+    type ActivateSemantics() =
+
+        member x.Activate(r : Root<ISg>, scope : Ag.Scope) =
+            r.Child?Activate <- ([] : list<unit -> IDisposable>)
+
+        member x.Activate(o : Sg.ActivationApplicator, scope : Ag.Scope) =
+            o.Child?Activate <- o.Activate :: scope.Activate
