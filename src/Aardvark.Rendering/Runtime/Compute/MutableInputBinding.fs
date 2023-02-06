34e361f9e6e0d9a839820e62b6778427ad11e6ca
Martin <mautner@vrvis.at>
2023-02-06 16:46:34 +0100

    [Compute] Implement mutable input binding

--- src/Aardvark.Rendering/Runtime/Compute/MutableInputBinding.fs
+++ src/Aardvark.Rendering/Runtime/Compute/MutableInputBinding.fs
@@ -4,23 +4,109 @@ open FSharp.Data.Adaptive
 open Aardvark.Base
 
 open System
+open System.Reflection
+open System.Collections.Generic
 open System.Runtime.CompilerServices
+
+module private MutableComputeBindingInternals =
+
+    module AVal =
+
+        [<AutoOpen>]
+        module private Dispatch =
+
+            type private Dispatcher() =
+                static member Create<'T>() = AVal.init Unchecked.defaultof<'T>
+                static member Change(cval : ChangeableValue<'T>, value : 'T) = cval.Value <- value
+
+            module Method =
+                let private flags = BindingFlags.Static ||| BindingFlags.NonPublic ||| BindingFlags.Public
+                let create = typeof<Dispatcher>.GetMethod("Create", flags)
+                let change = typeof<Dispatcher>.GetMethod("Change", flags)
+
+        let create (contentType : Type) =
+            let mi = Method.create.MakeGenericMethod [| contentType |]
+            mi.Invoke(null, [||]) |> unbox<IAdaptiveValue>
+
+        let change (value : obj) (cval : IAdaptiveValue) =
+            let mi = Method.change.MakeGenericMethod [| cval.ContentType |]
+            mi.Invoke(null, [| cval; value |]) |> ignore
+
+    // When an input is first set, the provided value might be of a specialized type.
+    // The type of that value determines the content type of cval value that is created at that point.
+    // This might be troublesome if a highly specialized value is set and a more general one afterwards.
+    let private knownBaseTypes =
+        [| typeof<ITexture>
+           typeof<ITextureLevel>
+           typeof<IBuffer> |]
+
+    let getBaseType (value : obj) =
+        let typ = value.GetType()
+
+        knownBaseTypes |> Array.tryFind (fun baseType ->
+            baseType.IsAssignableFrom typ
+        )
+        |> Option.defaultValue typ
+
+    // Wrap arrays so they are compatible with buffers.
+    let inline wrapArray (value : obj) : obj =
+        match value with
+        | :? Array as arr -> ArrayBuffer(arr)
+        | _ -> value
+
+
+open MutableComputeBindingInternals
 
 /// Mutable variant for compute input bindings.
 /// For backwards-compatible only, new code should create input bindings from an IUniformProvider.
 type MutableComputeInputBinding internal(shader : IComputeShader) =
+    let cvals = Dictionary<string, IAdaptiveValue>()
+    let pending = Dictionary<string, obj>()
+
+    member x.TryGetValue(name : string) =
+        lock x (fun _ ->
+            match cvals.TryGetValue(name) with
+            | (true, cval) -> Some cval
+            | _ -> None
+        )
 
     member x.Item
-        with set (name : string) (value : obj) = ()
+        with set (name : string) (value : obj) =
+            lock x (fun _ ->
+                let wrapped = wrapArray value
+
+                match cvals.TryGetValue name with
+                | (true, cval) ->
+                    if not <| cval.ContentType.IsAssignableFrom(wrapped.GetType()) then
+                        raise <| ArgumentException($"Cannot set input {name} (type = {cval.ContentType}) to value of type {value.GetType()}.")
+                | _ ->
+                    let baseType = getBaseType wrapped
+                    cvals.[name] <- AVal.create baseType
+
+                pending.[name] <- wrapped
+            )
 
     member x.Flush() =
-        ()
+        lock x (fun _ ->
+            if pending.Count > 0 then
+                transact (fun _ ->
+                    for KeyValue(name, value) in pending do
+                        cvals.[name] |> AVal.change value
+                )
+                pending.Clear()
+        )
 
     member x.Dispose() =
-        ()
+        lock x (fun _ ->
+            cvals.Clear()
+            pending.Clear()
+        )
 
     interface IComputeInputBinding with
         member x.Shader = shader
+
+    interface IUniformProvider with
+        member x.TryGetUniform(_scope, name) = x.TryGetValue(string name)
 
     interface IDisposable with
         member x.Dispose() = x.Dispose()
---
+++ src/Tests/Aardvark.Rendering.Tests/Tests/Compute/MutableInputBinding.fs
@@ -0,0 +1,86 @@
+﻿namespace Aardvark.Rendering.Tests.Rendering
+
+open Aardvark.Base
+open Aardvark.Rendering
+open Aardvark.Rendering.Tests
+open Aardvark.Application
+open FSharp.Data.Adaptive
+open Expecto
+open System
+
+module MutableInputBinding =
+
+    module Cases =
+
+        let private createDummyShader (runtime : IRuntime) =
+            { new IComputeShader with
+                member x.Runtime = runtime
+                member x.LocalSize = V3i.One
+                member x.Dispose() = () }
+
+        let private flushTest'<'T when 'T : equality> (value1 : obj) (test1 : obj) (value2 : obj) (test2 : obj) (runtime : IRuntime) =
+            use shader = createDummyShader runtime
+            use inputs = runtime.NewInputBinding(shader)
+
+            inputs.["test"] <- value1
+
+            let aval =
+                let v = inputs.TryGetValue("test").Value
+                Expect.equal v.ContentType typeof<'T> "Unexpected aval content type"
+                unbox<aval<'T>> v
+
+            Expect.equal (aval.GetValue()) Unchecked.defaultof<'T> "Unexpected default value"
+
+            inputs.Flush()
+
+            Expect.equal (aval.GetValue()) (unbox<'T> test1) "Unexpected value after first flush"
+
+            inputs.["test"] <- value2
+            inputs.Flush()
+
+            Expect.equal (aval.GetValue()) (unbox<'T> test2) "Unexpected value after second flush"
+
+        let private flushTest<'T when 'T : equality> (value1 : obj) (value2 : obj) =
+            flushTest'<'T> value1 value1 value2 value2
+
+        let flush =
+            flushTest<int> 42 64
+
+        let typeSafety (runtime : IRuntime) =
+            use shader = createDummyShader runtime
+            use inputs = runtime.NewInputBinding(shader)
+
+            inputs.["test"] <- 54
+            Expect.equal (inputs.TryGetValue("test").Value.ContentType) typeof<int32> "Unexpected aval content type"
+
+            Expect.throwsT<ArgumentException> (fun _ ->
+                inputs.["test"] <- 54.0
+            ) "Type check failed"
+
+        let buffers (runtime : IRuntime) =
+            use backendBuffer = runtime.CreateBuffer(128n)
+            let array = [| 12; 13 |]
+            let arrayBuffer = ArrayBuffer array
+            runtime |> flushTest<IBuffer> backendBuffer arrayBuffer
+            runtime |> flushTest'<IBuffer> backendBuffer backendBuffer array arrayBuffer
+
+        let textures (runtime : IRuntime) =
+            use backendTexture = runtime.CreateTexture1D(42, TextureFormat.Rgba8)
+            let fileTexture = FileTexture("/dev/null")
+            runtime |> flushTest<ITexture> backendTexture fileTexture
+
+        let textureLevels (runtime : IRuntime) =
+            use backendTexture = runtime.CreateTexture1D(42, TextureFormat.Rgba8)
+            let subres = backendTexture.[TextureAspect.Color, 0, 0]
+            let level = backendTexture.[TextureAspect.Color, 0]
+            runtime |> flushTest<ITextureLevel> subres level
+
+    let tests (backend : Backend) =
+        [
+            "Flush",          Cases.flush
+            "Type safety",    Cases.typeSafety
+            "Buffers",        Cases.buffers
+            "Textures",       Cases.textures
+            "Texture levels", Cases.textureLevels
+        ]
+        |> prepareCases backend "Mutable input binding"
