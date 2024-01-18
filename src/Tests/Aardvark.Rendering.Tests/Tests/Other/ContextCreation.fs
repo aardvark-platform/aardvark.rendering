7a6d181aa1927db7acad9048fca12bcefef9abd0
Martin <mautner@vrvis.at>
2024-01-18 11:54:22 +0100

    [Tests] Improve context sharing tests

--- src/Tests/Aardvark.Rendering.Tests/Tests/Other/ContextCreation.fs
+++ src/Tests/Aardvark.Rendering.Tests/Tests/Other/ContextCreation.fs
@@ -14,30 +14,53 @@ module ``ContextCreation Tests`` =
 
     module Cases =
 
-        let private checkShared (h : ContextHandle) (ctx : Context) =
+        module private TestTexture =
 
-            let createTexture() =
-                use __ = ctx.ResourceLock
+            let private textureData : uint8[] =
+                Array.init 16 uint8
+
+            let create() =
                 let t = GL.GenTexture()
                 GL.Check "failed to create texture"
+
+                GL.BindTexture(TextureTarget.Texture1D, t)
+                GL.Check "failed to bind texture"
+
+                GL.TexImage1D(TextureTarget.Texture1D, 0, PixelInternalFormat.R8ui, textureData.Length, 0, PixelFormat.RedInteger, PixelType.UnsignedByte, textureData)
+                GL.Check "failed to allocate texture"
+
                 t
 
-            let t = createTexture()
+            let getAndCheck (texture : int) =
+                GL.BindTexture(TextureTarget.Texture1D, texture)
+                GL.Check "failed to bind texture"
 
-            use __ = ctx.RenderingLock h
+                let result = Array.zeroCreate<uint8> textureData.Length
+                GL.GetTexImage(TextureTarget.Texture1D, 0, PixelFormat.RedInteger, PixelType.UnsignedByte, result)
+                GL.Check "failed to get texture image"
 
-            GL.BindTexture(TextureTarget.Texture2D, t)
-            GL.Check "failed to bind texture"
+                if result <> textureData then
+                    failwithf "Data retrieved from texture is wrong: %A" result
 
-            GL.DeleteTexture t
-            GL.Check "failed to delete texture"
+        module private TextureSharing =
+
+            let check (ctx : Context) (handle : ContextHandle) =
+                let texture =
+                    use __ = ctx.ResourceLock
+                    TestTexture.create()
+
+                use __ = ctx.RenderingLock handle
+                TestTexture.getAndCheck texture
+
+                GL.DeleteTexture texture
+                GL.Check "failed to delete texture"
 
         let createAfterDispose (runtime : IRuntime) =
             let ctx = (unbox<Runtime> runtime).Context
 
             ctx.CreateContext().Dispose()
             use h = ctx.CreateContext()
-            checkShared h ctx
+            TextureSharing.check ctx h
 
         let createAfterMakeCurrent (runtime : IRuntime) =
             let ctx = (unbox<Runtime> runtime).Context
@@ -60,19 +83,62 @@ module ``ContextCreation Tests`` =
             try
                 event.Wait()
                 use h = ctx.CreateContext()
-                checkShared h ctx
+                TextureSharing.check ctx h
 
             finally
                 stop <- true
                 thread.Join()
+
+        let createAfterMakeCurrentAll (runtime : IRuntime) =
+            let ctx = (unbox<Runtime> runtime).Context
+
+            let t =
+                use __ = ctx.ResourceLock
+                TestTexture.create()
+
+            let count = RuntimeConfig.NumberOfResourceContexts
+            let mutable stop = false
+            use semaphore = new SemaphoreSlim(0, count)
+
+            let threads =
+                Array.init count (fun _ ->
+                    startThread (fun _ ->
+                        use __ = ctx.ResourceLock
+
+                        semaphore.Release() |> ignore
+
+                        while not stop do
+                            Thread.Sleep 10
+                    )
+                )
+
+            try
+                for _ = 1 to count do
+                    semaphore.Wait()
+
+                use h = ctx.CreateContext()
+                use __ = ctx.RenderingLock h
+
+                TestTexture.getAndCheck t
+
+                GL.DeleteTexture t
+                GL.Check "failed to delete texture"
+
+            finally
+                stop <- true
+                for t in threads do
+                    t.Join()
 
     let private testsWithFramework (framework : Framework) =
         [
             // Tests context sharing and demonstrates that using the
             // last context as parent is not a good strategy. When sharing the
             // parent context must exist (duh!) and not be current on another thread.
-            "Create after dispose",      Cases.createAfterDispose
-            "Create after make current", Cases.createAfterMakeCurrent
+            "Create after dispose",             Cases.createAfterDispose
+            "Create after make current",        Cases.createAfterMakeCurrent
+
+            // Worst case scenario in which all contexts are currently used.
+            "Create after make current all",    Cases.createAfterMakeCurrentAll
         ]
         |> List.map (fun (name, test) ->
             let name = $"[{framework}] {name}"
