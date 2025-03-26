411e3f80d615bd940c54cf3d7dd48cabc8804dbf
Martin <mautner@vrvis.at>
2025-03-26 15:50:40 +0100

    [Vulkan] Fix VMA Windows support

--- src/vkvm/vma.cpp
+++ src/vkvm/vma.cpp
@@ -1,9 +1,17 @@
 #ifndef __GNUC__
 #include "stdafx.h"
+#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan_win32.h>
 #define VMA_CALL_PRE  __declspec(dllexport)
 #define VMA_CALL_POST __cdecl
 #endif
 
+#ifdef _DEBUG
+#define VMA_DEBUG_LOG_FORMAT(format, ...) do { \
+   printf((format), __VA_ARGS__); \
+   printf("\n"); \
+} while(false)
+#endif
+
 #define VMA_IMPLEMENTATION
-#define VMA_VULKAN_VERSION 1003000
 #include <vma/vk_mem_alloc.h>
