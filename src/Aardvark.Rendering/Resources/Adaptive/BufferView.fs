3d1127c7ca1f55a54289edd11c6a84e19217f90a
Martin <mautner@vrvis.at>
2025-06-18 17:01:07 +0200

    Use IBuffer.ToArray in BufferView.download

--- src/Aardvark.Rendering/Resources/Adaptive/BufferView.fs
+++ src/Aardvark.Rendering/Resources/Adaptive/BufferView.fs
@@ -66,84 +66,15 @@ module BufferView =
     let ofArray (arr : Array) =
         BufferView(arr)
 
-    [<AbstractClass>]
-    type private Reader() =
-        static let cache = Dict<Type, Reader>()
-
-        static member Get (t : Type) =
-            cache.GetOrCreate(t, fun t ->
-                let rt = typedefof<Reader<int>>.MakeGenericType [|t|]
-                let instance = Activator.CreateInstance rt
-                instance |> unbox<Reader>
-            )
-
-        abstract member Read : ptr : nativeint * count : int * stride : int -> Array
-        abstract member Initialize : 'a * int -> Array
-
-    and private Reader<'a when 'a : unmanaged>() =
-        inherit Reader()
-
-        override x.Read(ptr : nativeint, count : int, stride : int) =
-            let arr : 'a[] = Array.zeroCreate count
-
-            if stride = 0 || stride = sizeof<'a> then
-                let size = count * sizeof<'a>
-                arr |> NativePtr.pinArr (fun pDst ->
-                    NativeInt.memcpy ptr pDst.Address size
-                )
-            else
-                let step = nativeint stride
-                let mutable current = ptr
-                for i in 0 .. count - 1 do
-                    arr.[i] <- NativeInt.read current
-                    current <- current + step
-
-            arr :> Array
-
-        override x.Initialize (value : 'x, count : int) =
-            let conv = Aardvark.Base.PrimitiveValueConverter.converter<'x, 'a>
-            Array.create count (conv value) :> Array
-
-    let download (startIndex : int) (count : int) (view : BufferView) =
-        let elementType = view.ElementType
-        let elementSize = Marshal.SizeOf elementType
-        let reader = Reader.Get elementType
-        let offset = view.Offset + elementSize * startIndex
-
-        let stride =
-            if view.Stride = 0 then elementSize
-            else view.Stride
-
+    let download (startIndex : int) (count : int) (view : BufferView) : aval<Array> =
         match view.SingleValue with
         | Some value ->
             value.Accept (
                 { new IAdaptiveValueVisitor<_> with
-                    member x.Visit(value : aval<'T>) =
-                        value |> AVal.map (fun v -> reader.Initialize(v, count))
+                    member x.Visit(value) = value |> AVal.map (fun v -> Array.replicate count v :> Array)
                 }
             )
 
         | _ ->
-            view.Buffer |> AdaptiveResource.map (fun b ->
-                match b with
-                | :? ArrayBuffer as a when stride = elementSize && offset = 0 ->
-                    if count = a.Data.Length then
-                        a.Data
-                    else
-                        if count > a.Data.Length then
-                            raise <| IndexOutOfRangeException("[BufferView] trying to download too many elements from ArrayBuffer")
-
-                        let res = Array.CreateInstance(elementType, count)
-                        Array.Copy(a.Data, res, count)
-                        res
-
-                | :? INativeBuffer as b ->
-                    let available = (b.SizeInBytes - nativeint view.Offset) / nativeint elementSize
-                    if count > int available then
-                        raise <| IndexOutOfRangeException("[BufferView] trying to download too many elements from NativeBuffer")
-
-                    b.Use (fun ptr -> reader.Read(ptr + nativeint offset, count, stride))
-
-                | _ ->
-                    failwith "[BufferView] unknown buffer-type"
-            )
+            let offset = uint64 view.Offset + uint64 view.ElementType.CLRSize * uint64 startIndex
+            view.Buffer |> AdaptiveResource.map (_.ToArray(view.ElementType, uint64 count, offset, uint64 view.Stride))
