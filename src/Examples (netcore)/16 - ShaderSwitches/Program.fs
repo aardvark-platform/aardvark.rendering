b1cfcbb23d7409c920b07a7553ba9b56086657ec
Martin <mautner@vrvis.at>
2025-09-23 14:01:45 +0200

    [Examples] Add GUI for shader switch example

--- src/Examples (netcore)/16 - ShaderSwitches/Program.fs
+++ src/Examples (netcore)/16 - ShaderSwitches/Program.fs
@@ -1,5 +1,6 @@
 ﻿open Aardvark.Base
 open Aardvark.Rendering
+open Aardvark.Rendering.ImGui
 open FSharp.Data.Adaptive
 open Aardvark.SceneGraph
 open Aardvark.Application
@@ -8,66 +9,73 @@ open FShade
 // This example illustrates how to use dynamic shaders.
 
 [<EntryPoint>]
-let main argv =
+let main _argv =
 
     // first we need to initialize Aardvark's core components
     Aardvark.Init()
 
-    let win =
+    use win =
         window {
             backend Backend.GL
             display Display.Mono
             debug true
             samples 8
+            showHelp false
+            initialCamera (CameraView.lookAt (V3d(2.0, 2.0, 1.5)) V3d.Zero V3d.OOI)
         }
+
+    use gui = win.Control.InitializeImGui()
 
     let activeShader = AVal.init 0
 
     let effects =
         [|
             // red
-            Effect.compose [
+            "Constant color (flat)", Effect.compose [
                 toEffect DefaultSurfaces.trafo
                 toEffect (DefaultSurfaces.constantColor C4f.Red)
             ]
 
             // red with lighting
-            Effect.compose [
+            "Constant color", Effect.compose [
                 toEffect DefaultSurfaces.trafo
                 toEffect (DefaultSurfaces.constantColor C4f.Red)
                 toEffect DefaultSurfaces.simpleLighting
             ]
 
             // vertex colors with lighting
-            Effect.compose [
+            "Vertex color", Effect.compose [
                 toEffect DefaultSurfaces.trafo
                 toEffect DefaultSurfaces.simpleLighting
             ]
 
             // texture with lighting
-            Effect.compose [
+            "Textured", Effect.compose [
                 toEffect DefaultSurfaces.trafo
                 toEffect DefaultSurfaces.diffuseTexture
                 toEffect DefaultSurfaces.simpleLighting
             ]
         |]
 
-
-    win.Keyboard.DownWithRepeats.Values.Add (fun k ->
-        match k with
-            | Keys.Enter ->
-                transact (fun () -> activeShader.Value <- (activeShader.Value + 1) % effects.Length)
-            | _ ->
-                ()
-    )
+    gui.Render <- fun () ->
+        if ImGui.Begin("Settings", ImGuiWindowFlags.AlwaysAutoResize) then
+            let active, _ = effects.[activeShader.Value]
+            if ImGui.BeginCombo("##shader", active) then
+                for i = 0 to effects.Length - 1 do
+                    if ImGui.Selectable(fst effects.[i], (activeShader.Value = i)) then
+                        activeShader.Value <- i
+                ImGui.EndCombo()
+        ImGui.End()
 
     let sg =
+        let effects = effects |> Array.map snd
+
         Sg.box' C4b.Green Box3d.Unit
             |> Sg.diffuseTexture DefaultTextures.checkerboard
             |> Sg.effectPool effects activeShader
 
     // show the scene in a simple window
-    win.Scene <- sg
+    win.Scene <- RenderCommand.Ordered [sg; gui] |> Sg.execute
     win.Run()
 
     0
